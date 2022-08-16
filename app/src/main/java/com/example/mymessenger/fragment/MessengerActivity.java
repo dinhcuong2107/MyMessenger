5c819fd2b8009d4946023f0b03e6679be7fbb946
Admin <dinhcuong.kos.2107@gmail.com>
2022-08-16 18:01:17 +0700

    update

--- app/src/main/java/com/example/mymessenger/fragment/MessengerActivity.java
+++ app/src/main/java/com/example/mymessenger/fragment/MessengerActivity.java
@@ -2,6 +2,7 @@ package com.example.mymessenger.fragment;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +12,47 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.mymessenger.R;
 import com.google.firebase.database.DatabaseReference;
 
-public class MessengerActivity extends Fragment {
+public class MessengerActivity extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 String key_user="";
 DatabaseReference databaseReferenceMessenger,databaseReference;
 EditText editText_find_user;
 ProgressDialog progressDialog;
 RecyclerView recyclerViewMessenger;
+SwipeRefreshLayout swipeRefreshLayout;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_messenger,container,false);
-
-
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_messenger);
         editText_find_user = (EditText) view.findViewById(R.id.frag_messenger_edt_search);
         recyclerViewMessenger = (RecyclerView) view.findViewById(R.id.frag_messenger_recyclerview);
 
         Bundle bundle = getActivity().getIntent().getExtras();
         key_user = bundle.getString("key_user");
 
+        load_list_chat();
+        swipeRefreshLayout.setOnRefreshListener(this);
+
         return view;
+    }
+
+    private void load_list_chat() {
+    }
+
+    @Override
+    public void onRefresh() {
+        editText_find_user.setText("");
+        load_list_chat();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 1000);
     }
 }
